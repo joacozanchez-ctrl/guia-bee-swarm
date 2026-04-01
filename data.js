109a5cbda6e96b5f07cf1ed6c9ffd8fc701fcffb
joacozanchez-ctrl <joacozanchez@gmail.com>
2026-04-01 20:09:16 -0300

    Update data.js

--- data.js
+++ data.js
@@ -2,54 +2,48 @@ const itemsData = [
 
     { 
         name: "Strawberry",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/7/74/Strawberry.png"
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/7/74/Strawberry.png/revision/latest"
     },
     { 
         name: "Blueberry",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/4/44/Blueberry.png"
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/4/44/Blueberry.png/revision/latest"
     },
     { 
         name: "Royal Jelly",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/e/e5/Royal_Jelly.png"
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/e/e5/Royal_Jelly.png/revision/latest"
     },
     { 
         name: "Sunflower Seeds",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/3/36/Sunflower_Seed.png"
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/3/36/Sunflower_Seed.png/revision/latest"
     },
     { 
         name: "Pineapple",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/6/60/Pineapple.png"
-    },
-    { 
-        name: "Gumdrops",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/0/0c/Gumdrops.png"
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/6/60/Pineapple.png/revision/latest"
     },
     { 
         name: "Coconut",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/f/f3/Coconut.png"
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/f/f3/Coconut.png/revision/latest"
     },
 
     {
         name: "Oil",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/3/3b/Oil.png",
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/3/3b/Oil.png/revision/latest",
         recipe: {
             "Sunflower Seeds": 50,
             "Royal Jelly": 10
         }
     },
-
     {
         name: "Enzymes",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/5/5f/Enzymes.png",
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/5/5f/Enzymes.png/revision/latest",
         recipe: {
             "Pineapple": 50,
             "Royal Jelly": 10
         }
     },
-
     {
         name: "Tropical Drink",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/7/70/Tropical_Drink.png",
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/7/70/Tropical_Drink.png/revision/latest",
         recipe: {
             "Coconut": 10,
             "Enzymes": 2,
