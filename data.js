0f9ffa92534b7a68e6aea278837ce5c4f9f6b663
joacozanchez-ctrl <joacozanchez@gmail.com>
2026-04-01 19:04:34 -0300

    Update data.js

--- data.js
+++ data.js
@@ -9,49 +9,64 @@ const itemsData = [
     { name: "Gumdrops" },
     { name: "Coconut" },
 
-    // CRAFTEOS
-    {
-        name: "Red Extract",
-        recipe: {
-            "Strawberry": 50,
-            "Royal Jelly": 10
-        }
-    },
-    {
-        name: "Blue Extract",
-        recipe: {
-            "Blueberry": 50,
-            "Royal Jelly": 10
-        }
-    },
-    {
-        name: "Oil",
-        recipe: {
-            "Sunflower Seeds": 50,
-            "Royal Jelly": 10
-        }
-    },
-    {
-        name: "Enzymes",
-        recipe: {
-            "Pineapple": 50,
-            "Royal Jelly": 10
-        }
-    },
-    {
-        name: "Glue",
-        recipe: {
-            "Gumdrops": 50,
-            "Royal Jelly": 10
-        }
-    },
-    {
-        name: "Tropical Drink",
-        recipe: {
-            "Coconut": 10,
-            "Enzymes": 2,
-            "Oil": 2
-        }
-    }
+   // ===== CRAFTEOS REALES =====
 
-];
+{
+    name: "Red Extract",
+    recipe: {
+        "Strawberry": 50,
+        "Royal Jelly": 10
+    }
+},
+{
+    name: "Blue Extract",
+    recipe: {
+        "Blueberry": 50,
+        "Royal Jelly": 10
+    }
+},
+{
+    name: "Oil",
+    recipe: {
+        "Sunflower Seeds": 50,
+        "Royal Jelly": 10
+    }
+},
+{
+    name: "Enzymes",
+    recipe: {
+        "Pineapple": 50,
+        "Royal Jelly": 10
+    }
+},
+{
+    name: "Glue",
+    recipe: {
+        "Gumdrops": 50,
+        "Royal Jelly": 10
+    }
+},
+// ===== CORREGIDOS =====
+
+{
+    name: "Glitter",
+    recipe: {
+        "Moon Charm": 25,
+        "Magic Bean": 1
+    }
+},
+{
+    name: "Star Jelly",
+    recipe: {
+        "Royal Jelly": 100,
+        "Glitter": 3
+    }
+},
+{
+    name: "Tropical Drink",
+    recipe: {
+        "Coconut": 10,
+        "Enzymes": 2,
+        "Oil": 2
+    }
+}
