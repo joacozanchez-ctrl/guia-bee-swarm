b5d190f1518797b2b69b363f64d01b4d75a8ddf9
joacozanchez-ctrl <joacozanchez@gmail.com>
2026-04-01 20:17:21 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,33 +1,15 @@
 const itemsData = [
 
-    { 
-        name: "Strawberry",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/7/74/Strawberry.png/revision/latest"
-    },
-    { 
-        name: "Blueberry",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/4/44/Blueberry.png/revision/latest"
-    },
-    { 
-        name: "Royal Jelly",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/e/e5/Royal_Jelly.png/revision/latest"
-    },
-    { 
-        name: "Sunflower Seeds",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/3/36/Sunflower_Seed.png/revision/latest"
-    },
-    { 
-        name: "Pineapple",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/6/60/Pineapple.png/revision/latest"
-    },
-    { 
-        name: "Coconut",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/f/f3/Coconut.png/revision/latest"
-    },
+    { name: "Strawberry", emoji: "🍓" },
+    { name: "Blueberry", emoji: "🫐" },
+    { name: "Royal Jelly", emoji: "🍯" },
+    { name: "Sunflower Seeds", emoji: "🌻" },
+    { name: "Pineapple", emoji: "🍍" },
+    { name: "Coconut", emoji: "🥥" },
 
     {
         name: "Oil",
-        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/3/3b/Oil.png/revision/latest",
+        emoji: "🛢️",
         recipe: {
             "Sunflower Seeds": 50,
             "Royal Jelly": 10
@@ -35,6 +17,23 @@ const itemsData = [
     },
     {
         name: "Enzymes",
+        emoji: "🧪",
+        recipe: {
+            "Pineapple": 50,
+            "Royal Jelly": 10
+        }
+    },
+    {
+        name: "Tropical Drink",
+        emoji: "🍹",
+        recipe: {
+            "Coconut": 10,
+            "Enzymes": 2,
+            "Oil": 2
+        }
+    }
+
+];        name: "Enzymes",
         img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/5/5f/Enzymes.png/revision/latest",
         recipe: {
             "Pineapple": 50,
