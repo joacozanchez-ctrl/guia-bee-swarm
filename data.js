8a0b3cca8fe2fd74ce8c4e5699ad90dd9669d93e
joacozanchez-ctrl <joacozanchez@gmail.com>
2026-04-01 19:57:35 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,66 +1,55 @@
 const itemsData = [
 
-    // MATERIALES
-    { name: "Strawberry" },
-    { name: "Blueberry" },
-    { name: "Royal Jelly" },
-    { name: "Sunflower Seeds" },
-    { name: "Pineapple" },
-    { name: "Gumdrops" },
-    { name: "Coconut" },
+    { 
+        name: "Strawberry",
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/7/74/Strawberry.png"
+    },
+    { 
+        name: "Blueberry",
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/4/44/Blueberry.png"
+    },
+    { 
+        name: "Royal Jelly",
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/e/e5/Royal_Jelly.png"
+    },
+    { 
+        name: "Sunflower Seeds",
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/3/36/Sunflower_Seed.png"
+    },
+    { 
+        name: "Pineapple",
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/6/60/Pineapple.png"
+    },
+    { 
+        name: "Gumdrops",
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/0/0c/Gumdrops.png"
+    },
+    { 
+        name: "Coconut",
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/f/f3/Coconut.png"
+    },
 
-    // CRAFTEOS
-    {
-        name: "Red Extract",
-        recipe: {
-            "Strawberry": 50,
-            "Royal Jelly": 10
-        }
-    },
-    {
-        name: "Blue Extract",
-        recipe: {
-            "Blueberry": 50,
-            "Royal Jelly": 10
-        }
-    },
     {
         name: "Oil",
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/3/3b/Oil.png",
         recipe: {
             "Sunflower Seeds": 50,
             "Royal Jelly": 10
         }
     },
+
     {
         name: "Enzymes",
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/5/5f/Enzymes.png",
         recipe: {
             "Pineapple": 50,
             "Royal Jelly": 10
         }
     },
-    {
-        name: "Glue",
-        recipe: {
-            "Gumdrops": 50,
-            "Royal Jelly": 10
-        }
-    },
-    {
-        name: "Glitter",
-        recipe: {
-            "Moon Charm": 25,
-            "Magic Bean": 1
-        }
-    },
-    {
-        name: "Star Jelly",
-        recipe: {
-            "Royal Jelly": 100,
-            "Glitter": 3
-        }
-    },
+
     {
         name: "Tropical Drink",
+        img: "https://static.wikia.nocookie.net/bee-swarm-simulator/images/7/70/Tropical_Drink.png",
         recipe: {
             "Coconut": 10,
             "Enzymes": 2,
