4c8c7e032d1ddad107b37008bf049f275c6ba611
joacozanchez-ctrl <joacozanchez@gmail.com>
2026-04-01 18:53:29 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,60 +1,57 @@
-// ===== BLENDER CORREGIDO =====
-{
-    name: "Red Extract",
-    recipe: {
-        Strawberry: 50,
-        "Royal Jelly": 10
-    }
-},
-{
-    name: "Blue Extract",
-    recipe: {
-        Blueberry: 50,
-        "Royal Jelly": 10
-    }
-},
-{
-    name: "Oil",
-    recipe: {
-        "Sunflower Seeds": 50,
-        "Royal Jelly": 10
-    }
-},
-{
-    name: "Enzymes",
-    recipe: {
-        Pineapple: 50,
-        "Royal Jelly": 10
-    }
-},
-{
-    name: "Glue",
-    recipe: {
-        Gumdrops: 50,
-        "Royal Jelly": 10
-    }
-},
+const itemsData = [
 
-// ===== IMPORTANTES CORREGIDOS =====
-{
-    name: "Glitter",
-    recipe: {
-        "Moon Charm": 25,
-        "Magic Bean": 1,
+    // MATERIALES
+    { name: "Strawberry" },
+    { name: "Blueberry" },
+    { name: "Royal Jelly" },
+    { name: "Sunflower Seeds" },
+    { name: "Pineapple" },
+    { name: "Gumdrops" },
+    { name: "Coconut" },
+
+    // CRAFTEOS
+    {
+        name: "Red Extract",
+        recipe: {
+            "Strawberry": 50,
+            "Royal Jelly": 10
+        }
+    },
+    {
+        name: "Blue Extract",
+        recipe: {
+            "Blueberry": 50,
+            "Royal Jelly": 10
+        }
+    },
+    {
+        name: "Oil",
+        recipe: {
+            "Sunflower Seeds": 50,
+            "Royal Jelly": 10
+        }
+    },
+    {
+        name: "Enzymes",
+        recipe: {
+            "Pineapple": 50,
+            "Royal Jelly": 10
+        }
+    },
+    {
+        name: "Glue",
+        recipe: {
+            "Gumdrops": 50,
+            "Royal Jelly": 10
+        }
+    },
+    {
+        name: "Tropical Drink",
+        recipe: {
+            "Coconut": 10,
+            "Enzymes": 2,
+            "Oil": 2
+        }
     }
-},
-{
-    name: "Star Jelly",
-    recipe: {
-        "Royal Jelly": 100,
-        Glitter: 3,
-    }
-},
-{
-    name: "Tropical Drink",
-    recipe: {
-        Coconut: 10,
-        Enzymes: 2,
-        Oil: 2,
-    }
-}
+
+];
